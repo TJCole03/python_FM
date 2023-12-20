16ed83da5a754191e9fc733c799bd532aacb500e
TJCole03 <tjc6254@gmail.com>
2023-12-20 07:15:35 -0500

    zip practice

--- slicing/slice.py
+++ slicing/slice.py
@@ -45,3 +45,12 @@ print('backwards_slice', backwards_slice)
 # 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 
 # 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 
 # 1, 0]
+
+#ZIP
+import random
+names = ['bob', 'jack', 'dick', 'willie']
+scores = [random.randint(0,10) for name in names]
+print(scores)
+for name, score in zip(names, scores):
+    print(f"{name} got a score of {score}")
+
