feef838271eda9168fa50853e39a4ec188e7b101
TJCole03 <tjc6254@gmail.com>
2023-12-24 06:22:33 -0500

    main_lib.py and other_program.py. made simple library to write reusable code

---
+++ other_program/other_program.py
@@ -0,0 +1,8 @@
+import main_lib
+
+name = "Fred"
+
+length = main_lib.name_length(name)
+lower_case = main_lib.lower_case_name(name)
+
+print(f"In my code, my length is {length} and my lower case name is: {lower_case}")
